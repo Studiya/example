7e72c6c020e830f7585b3c44f5f166048710c49c
Valera Kalinichenko <studiya3@gmail.com>
2023-11-03 15:17:42 +0200

    Initial project version

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- <main>
+    <TheWelcome />
+  </main> -->
+  <div>{{ foo }} {{ bar }}</div>
+</template>
+
+<script>
+// import TheWelcome from '../components/TheWelcome.vue'
+  export default {
+    data() {
+      return {
+        foo: 1,
+        qux: 2,
+      }
+    },
+    created() {
+      this.foo = 3;
+      console.log (this.foo);
+    },
+    mounted() {
+      this.$el;
+      this.foo = 2;
+      console.log (this.foo);
+    },
+    watch() {
+
+    },
+    computed: {
+      bar() {
+        return this.foo + this.qux;
+      }
+    }
+  }
+</script>
